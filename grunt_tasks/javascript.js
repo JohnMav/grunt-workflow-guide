833e4607db0c70235da9133955b25b304a4e0d35
JohnUdacity <johnathan@udacity.com>
2015-06-12 10:47:53 -0700

    javascript and sass grunt_tasks defined

--- grunt_tasks/javascript.js
+++ grunt_tasks/javascript.js
@@ -1,3 +1,32 @@
 /*
 This will contain our JavaScript grunt tasks 
  */
+
+module.exports = function (grunt, config) {
+	grunt.config.merge({
+		concat: {
+		     dist: {
+		       src: config.jsSrcDir + '*.js',
+		       dest: config.jsConcatDir + 'scripts.js'
+		     },
+		   },
+		   jshint: {
+		     options: {
+		       "eqeqeq": true,
+		     },
+		     all: [
+		       'Gruntfile.js',
+		       config.jsSrcDir + "*.js"
+		     ]
+		   },
+		   watch: {
+		     js: {
+		       files: "<%= concat.dist.src %>",
+		       tasks: [
+		         "jshint",
+		         "concat"
+		       ]
+		     }
+		   }
+	});
+};
--- grunt_tasks/sass.js
+++ grunt_tasks/sass.js
@@ -1,3 +1,31 @@
 /*
 This will contain our Sass grunt tasks
  */
+
+module.exports = function (grunt, config) {
+  grunt.config.merge({
+    sass: {
+      dist: {
+        src: config.scssDir + 'style.scss',
+        dest: config.cssDir + 'style.css'
+      }
+    },
+    csslint: {
+      strict: {
+        options: {
+          important: 2
+        },
+        src: "<%= sass.dist.dest %>"
+      }
+    },
+    watch: {
+      sass: {
+		files: config.scssDir + "**/*.scss",
+        tasks: [
+          "sass",
+          "csslint"
+        ]
+      }
+    }
+  });
+};
